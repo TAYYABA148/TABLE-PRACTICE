4e3df320621b45ef1ad75c6f46cded4c9ae65373
TAYYABA148 <tayyabakulsoom64@gmail.com>
2024-11-12 14:43:50 +0500

    Add files via upload

---
+++ happy.js
@@ -0,0 +1,12 @@
+let myArray=[1,'Tayyaba',{text:"something"}]
+
+console.log(Array.isArray(myArray))
+
+console.log(myArray.length);
+
+//myArray.splice(0,2)
+//console.log('myArray');
+
+myArray.push('blue');
+
+console.log(myArray[2]);
